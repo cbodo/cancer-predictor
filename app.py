84dad6e7045c48a6fca5203937a80a07e79c4f28
C Bodo <cbodo17@gmail.com>
2023-10-04 10:57:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask
 from flask import render_template
 from predictor import textboxes
+from textbox import TextBox
 
 app = Flask(__name__, template_folder='template')
+
+textboxes = [
+    TextBox('radius_mean', 0, True),                # radius_mean
+    TextBox('texture_mean', 0, True),               # texture_mean
+    TextBox('perimeter_mean', 0, True),             # perimeter_mean 
+    TextBox('area_mean', 0, True),                  # area_mean 
+    TextBox('smoothness_mean', 0, True),            # smoothness_mean 
+    TextBox('compactness_mean', 0, True),           # compactness_mean 
+    TextBox('concavity_mean', 0, True),             # concavity_mean 
+    TextBox('concave points_mean', 0, True),        # concave points_mean 
+    TextBox('symmetry_mean', 0, True),              # symmetry_mean 
+    TextBox('fractal_dimension_mean', 0, True)      # fractal_dimension_mean
+]
 
 @app.route('/')
 def index():
--- predictor.py
+++ predictor.py
@@ -2,20 +2,49 @@ import pandas as pd
 import numpy as np
 import random
 from sklearn import linear_model, metrics, model_selection
-from textbox import TextBox
+from columnnames import COLUMN_NAMES
 
-COLUMN_NAMES = ['diagnosis', 'radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean', 'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean', 'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se', 'compactness_se', 'concavity_se', 'concave points_se', 'symmetry_se', 'fractal_dimension_se', 'radius_worst', 'texture_worst', 'perimeter_worst', 'area_worst', 'smoothness_worst', 'compactness_worst', 'concavity_worst', 'concave points_worst', 'symmetry_worst', 'fractal_dimension_worst']
+# import csv
+url = "./Cancer_Data.csv"
+data = pd.read_csv(url)
+df = pd.DataFrame(data,columns=COLUMN_NAMES)
 
-textboxes = [
-    TextBox('radius_mean', 0, True),                # radius_mean
-    TextBox('texture_mean', 0, True),               # texture_mean
-    TextBox('perimeter_mean', 0, True),             # perimeter_mean 
-    TextBox('area_mean', 0, True),                  # area_mean 
-    TextBox('smoothness_mean', 0, True),            # smoothness_mean 
-    TextBox('compactness_mean', 0, True),           # compactness_mean 
-    TextBox('concavity_mean', 0, True),             # concavity_mean 
-    TextBox('concave points_mean', 0, True),        # concave points_mean 
-    TextBox('symmetry_mean', 0, True),              # symmetry_mean 
-    TextBox('fractal_dimension_mean', 0, True)      # fractal_dimension_mean
-]
+# convert target column from categorical to numerical
+df['diagnosis'].replace(['B', 'M'], [0, 1], inplace=True)
 
+# create logistic regression model with max iterations set to 10000
+log_model = linear_model.LogisticRegression(max_iter=10000)
+
+# define the independent (X) and dependent (y) sets 
+X = df.drop(columns=['diagnosis'])
+y = df[['diagnosis']].copy()
+
+# create the training and testing sets
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.333, random_state=41)
+
+# train the model
+log_model.fit(X_train.values,np.ravel(y_train))
+
+# generate predictions set
+y_pred_log = log_model.predict(X_test.values)
+
+ # Accuracy Analysis
+
+# Calculate Accuracy (where accuracy = correct predictions / total predictions)
+accuracy_score = f"Accuracy Score: {metrics.accuracy_score(y_test, y_pred_log)}"
+
+# Calculate Mean Squared Error
+# Measure of the difference between predicted and actual values.
+mse = f"Mean Squared Error: {metrics.mean_squared_error(y_test, y_pred_log)}"
+
+# Cross-validation. 
+y_array = np.ravel(y.values)
+k_folds = model_selection.KFold(n_splits = 5, shuffle=True)
+scores = model_selection.cross_val_score(log_model, X, y_array)
+scores_mean = f"Cross Validation Average Score: {scores.mean()}"
+
+# defines function to generate a random number for an attribute in a range from the min value in the dataset to the max value in the dataset for that attribute
+def generate_rand(attribute):
+    min_val = df[str(attribute)].describe()['mean']-2*df[str(attribute)].describe()['std']
+    max_val = df[str(attribute)].describe()['mean']+2*df[str(attribute)].describe()['std']
+    return random.uniform(min_val, max_val)
