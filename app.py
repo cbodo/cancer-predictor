8fe7e07046153629f236334c0dcda93f54568520
C Bodo <cbodo17@gmail.com>
2023-10-04 09:55:27 -0700

    Add index.html

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+
+app = Flask(__name__, template_folder='template')
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
